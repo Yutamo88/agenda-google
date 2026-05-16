a446a30fe43356c7542e65c083969145e2b3a2e6
yutamo <ing.alvarocastellanos@gmail.com>
2026-05-16 11:24:00 -0500

    se agrega un formulario y se modifica la manera de enviar los datos

--- funciones.js
+++ funciones.js
@@ -1,14 +1,24 @@
+const HOJA = SpreadsheetApp.openById("1t1CRgFGQTXJ4kh04CQjmD8i8Nxa1YPIxuQ0YDKiGAUM");
+
 function doGet() {
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda de contactos - Google Apps Script');
 
+}
+
+function doPost(datos) {
+    insertarContacto(datos.parameter.nombre, datos.parameter.correo);
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda de contactos - Google Apps Script');
 }
 
 function obtenerDatosHTML(nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent(); 
 }
 
+//Obtiene los datos de los contactos de la hoja
 function obtenerContactos() {
-    let hoja = SpreadsheetApp.openById("1t1CRgFGQTXJ4kh04CQjmD8i8Nxa1YPIxuQ0YDKiGAUM").getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    return datos;
+    return HOJA.getDataRange().getValues();
+}
+
+function insertarContacto(nombre, correo) {
+    HOJA.appendRow([nombre, correo]);
 }
